5f0b056b2609a7191d55630bd6f8b85ff1e50165
Beatrice Skarp <beatrice.skarp@gmail.com>
2016-06-14 21:29:21 +0200

    Added onResume()

--- app/src/main/java/com/example/skarparna/myapplication/MainActivity.java
+++ app/src/main/java/com/example/skarparna/myapplication/MainActivity.java
@@ -29,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
